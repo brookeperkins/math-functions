028dcbad1678b87d4dc860090b964ad1360d7bec
brookeperkins <brookemmperkins@gmail.com>
2020-07-12 13:49:01 -0700

    added SumArray functin

--- test/math-functions.test.js
+++ test/math-functions.test.js
@@ -2,9 +2,9 @@ import {
     sum, 
     multiply, 
     sumAndMultiplyThreeNumbers, 
-    // sumArrayWithThreeNumbers, 
-    // multiplyArrayWithThreeNumbers, 
-    // multiplyAnyArray
+    sumArrayWithThreeNumbers, 
+    multiplyArrayWithThreeNumbers, 
+    multiplyAnyArray
 } from '../src/math-functions.js';
 
 const test = QUnit.test;
@@ -34,12 +34,12 @@ test('sumAndMultiplyThreeNumbers', (assert) => {
     assert.equal(result[3], 'The product of 4 and 7 and 5 is 140.');
 });
 
-// test('sumArrayWithThreeNumbers', (assert) => {
-//     const result = sumArrayWithThreeNumbers([2, 3, 4]);
+test('sumArrayWithThreeNumbers', (assert) => {
+    const result = sumArrayWithThreeNumbers([2, 3, 4]);
 
-//     assert.equal(result[0], 9);
-//     assert.equal(result[1], '2,3,4 was passed in as an array of numbers, and 9 is their sum.');
-// });
+    assert.equal(result[0], 9);
+    assert.equal(result[1], '2,3,4 was passed in as an array of numbers, and 9 is their sum.');
+});
 
 // test('multiplyArrayWithThreeNumbers', (assert) => {
 //     const result = multiplyArrayWithThreeNumbers([2, 3, 4]);
